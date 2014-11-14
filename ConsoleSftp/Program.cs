048ce68bf766326e86da9aa900218e6c6e6450bd
Bryan Nehl <k0emt@dbbear.com>
2014-11-14 16:15:45 -0600

    Initial commit

---
+++ ConsoleSftp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+
+using Renci.SshNet;
+
+namespace ConsoleSftp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uploadFile();
+        }
+
+        public static void uploadFile()
+        {
+            string host = "HOSTNAME HERE";
+            string username = "USERNAME HERE";
+            string password = "PASSWORD HERE";
+            string localFileName = @"c:\temp\sftp-testing\sample.txt";
+            string localFilesDir = @"c:\temp\sftp-testing\";
+            string localZipFileName = @"c:\temp\sftp-testing-spike.zip";
+
+            zipSourceDirectory(localFilesDir, localZipFileName);
+
+            //doSftp(host, username, password, localZipFileName);
+        }
+
+        private static void zipSourceDirectory(string localFilesDir, string localZipFileName)
+        {
+            ZipFile.CreateFromDirectory(localFilesDir, localZipFileName);
+        }
+
+        private static void doSftp(string host, string username, string password, string localZipFileName)
+        {
+            string remoteFileName = System.IO.Path.GetFileName(localZipFileName);
+            using (var sftp = new SftpClient(host, username, password))
+            {
+                sftp.Connect();
+
+                using (var file = File.OpenRead(localZipFileName))
+                {
+                    sftp.UploadFile(file, remoteFileName);
+                }
+
+                sftp.Disconnect();
+            }
+        }
+    }
+}
---
+++ TransferService/TransferService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace TransferService
+{
+    public partial class TransferService : ServiceBase
+    {
+        Boolean transferringAllowed = false;
+        EventLog eventLog1 = new EventLog();
+
+        public TransferService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // event log starting up
+
+            // load up configuration settings
+            var appSettings = ConfigurationManager.AppSettings;
+            string sftpUser = appSettings["user"];
+
+            // event log 
+            if (!System.Diagnostics.EventLog.SourceExists("MyTransferService"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+                    "MyTransferService", "MyLog");
+            }
+            // configure the event log instance to use this source name
+            eventLog1.Source = "MyTransferService";
+            eventLog1.WriteEntry("In OnStart.");
+            eventLog1.WriteEntry("user: " + sftpUser);
+
+            // spin up a timer
+            // add an event to the timer -- just have it event log for now
+        }
+
+        protected override void OnStop()
+        {
+            // event log stopping
+            eventLog1.WriteEntry("In OnStop.");
+        }
+
+        protected override void OnPause()
+        {
+            // event log pausing
+            eventLog1.WriteEntry("In OnPause.");
+        }
+
+        protected override void OnContinue()
+        {
+            // event log continue
+            eventLog1.WriteEntry("In OnContinue.");
+        }
+    }
+}
